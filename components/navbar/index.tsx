579cd4ae80e4bcf32a61ac2e344b0397149bbdae
fomes <fgomesdeluna@gmail.com>
2024-07-13 14:20:56 -0300

    feat: include header v1

---
+++ components/navbar/index.tsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+  return (
+    <header className="sticky top-0 z-10 shadow bg-slate-300">
+      <div className="container ms-auto p-4 flex items-center justify-between">
+        <span>LA Products</span>
+        <div className="flex items-center justify-center space-x-4">
+          <div className="rounded-full flex justify-center items-center w-8 h-8 bg-blue-400 cursor-pointer">
+            A
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
