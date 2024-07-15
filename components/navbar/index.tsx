e5f4ebe9cf1196c2b8d1357c527c82fac43861eb
fomes <fgomesdeluna@gmail.com>
2024-07-15 11:54:28 -0300

    fix: update navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -2,7 +2,7 @@ import { UserMenu } from "../user-menu";
 
 export function Navbar() {
   return (
-    <header className="sticky top-0 z-10 shadow bg-slate-300">
+    <header className="sticky top-0 z-10 shadow bg-slate-300 px-12">
       <div className="container ms-auto p-4 flex items-center justify-between">
         <span>LA Products</span>
         <div className="flex items-center justify-center space-x-4">
